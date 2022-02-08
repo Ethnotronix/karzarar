418978075e50da0b04723e749f4794b50894c8cc
Ali Can Kaya <alican_kaya@hotmail.com.tr>
2022-02-08 15:22:38 +0300

    refactor: path

--- main.js
+++
@@ -1,5 +0,0 @@
-const date = new Date().toISOString().split('T')[0]
-console.log(date)
-document.getElementById('sale-date').value = date
-document.getElementById('sale-date').max = date
-document.getElementById('buy-date').max = date
