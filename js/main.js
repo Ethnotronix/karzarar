8e765fb12d1c866549c5deb1629dbc489f1b26e4
Ali Can Kaya <alican_kaya@hotmail.com.tr>
2022-02-11 23:09:30 +0300

    feat: getUsdTry function multiple arg

--- js/main.js
+++ js/main.js
@@ -1,28 +1,13 @@
-const date = new Date().toISOString().split("T")[0];
+// helper functions
 
-const buyDateId = getById("buy-date");
-const saleDateId = getById("sale-date");
-const buttonReset = getById("reset-button");
-const buttonCalculate = getById("calculate-button");
-const resultContainer = document.querySelector(".result-container");
-const resultContainerContext = document.querySelector(".result-container>div");
-const calculateContainer = document.querySelector(".calculate-container");
-const buyPriceInputContainer = getById("buy-price-container");
-const salePriceInputContainer = getById("sale-price-container");
-
-saleDateId.value = date;
-saleDateId.max = date;
-buyDateId.max = date;
-
-async function getUsdTry(date) {
-  const response = await fetch("usdtry.json");
-  const data = await response.json();
-  return data[date];
+function getById(id) {
+  return document.getElementById(id);
 }
 
 function dateFormatter(date) {
   return (
-    date.split("-")[2] + "." + date.split("-")[1] + "." + date.split("-")[0]
+    // reverse date
+    date.split("-").reverse().join(".")
   );
 }
 
@@ -31,27 +16,63 @@ function numberFormatter(number) {
     maximumFractionDigits: 2,
   });
 }
+// startAnimate function can replay animate
+function startAnimate(id) {
+  id.classList.remove("error");
+  setTimeout(function () {
+    id.classList.add("error");
+  }, 1);
+}
+
+const date = new Date().toISOString().split("T")[0];
+
+const buttonReset = getById("reset-button");
+const buttonCalculate = getById("calculate-button");
+const resultContainer = document.querySelector(".result-container");
+const resultContainerContext = document.querySelector(".result-container>div");
+const calculateContainer = document.querySelector(".calculate-container");
+const buyPriceInputContainer = getById("buy-price-container");
+const salePriceInputContainer = getById("sale-price-container");
+
+const inputIds = {
+  buyDate: getById("buy-date"),
+  saleDate: getById("sale-date"),
+  buyPrice: getById("buy-price"),
+  salePrice: getById("sale-price")
+}
+
+inputIds.saleDate.value = date;
+inputIds.saleDate.max = date;
+inputIds.buyDate.max = date;
+
+async function getUsdTry(buyDate, saleDate) {
+  const response = await fetch("usdtry.json");
+  const data = await response.json();
+  return { 
+    buy: Number(data[buyDate]),
+    sale: Number(data[saleDate])
+  }
+}
 
 async function calculate(buyDate, saleDate, buyPrice, salePrice) {
-  const usdBuyDate = Number(await getUsdTry(buyDate));
-  const usdSaleDate = Number(await getUsdTry(saleDate));
-  if (!usdBuyDate) {
+  const usdTry = await getUsdTry(buyDate, saleDate);
+  if (!usdTry.buy) {
     return `<p class="summary">Alış tarihi için fiyat bilgisi mevcut değil!</p>`;
   }
-  if (!usdSaleDate) {
+  if (!usdTry.sale) {
     return `<p class="summary">Satış tarihi için fiyat bilgisi mevcut değil!</p>`;
   }
-  const usdBuyPrice = buyPrice / usdBuyDate;
-  const usdSalePrice = salePrice / usdSaleDate;
+  const usdBuyPrice = buyPrice / usdTry.buy;
+  const usdSalePrice = salePrice / usdTry.sale;
   const result = usdSalePrice - usdBuyPrice;
   const percentage = (Math.abs(result) / usdBuyPrice) * 100;
   let resultText = `<p>
   Ürünü aldığın <span>${dateFormatter(buyDate)}</span> tarihinde dolar kuru
-  <span>₺${usdBuyDate.toFixed(2)}</span> seviyesindeydi.<br />
+  <span>₺${numberFormatter(usdTry.buy)}</span> seviyesindeydi.<br />
   Alış fiyatın olan <span>₺${numberFormatter(Number(buyPrice))}</span> ile
   <span>$${numberFormatter(Number(usdBuyPrice))}</span> alabilirdin.<br />
   Sattığın <span>${dateFormatter(saleDate)}</span> tarihinde ise dolar kuru
-  <span>₺${usdSaleDate.toFixed(2)}</span> seviyesindeydi.<br />
+  <span>₺${numberFormatter(usdTry.sale)}</span> seviyesindeydi.<br />
    Satış fiyatın olan <span>₺${numberFormatter(
      Number(salePrice)
    )}</span> ile <span>$${numberFormatter(
@@ -79,10 +100,6 @@ async function calculate(buyDate, saleDate, buyPrice, salePrice) {
   return resultText;
 }
 
-function getById(id) {
-  return document.getElementById(id);
-}
-
 buttonReset.addEventListener("click", resetButtonClick);
 async function resetButtonClick() {
   resultContainer.style.display = "none";
@@ -91,22 +108,22 @@ async function resetButtonClick() {
 }
 buttonCalculate.addEventListener("click", calculateButtonClick);
 async function calculateButtonClick() {
-  const buyDate = buyDateId.value;
-  const saleDate = saleDateId.value;
+  const buyDate = inputIds.buyDate.value;
+  const saleDate = inputIds.saleDate.value;
   const buyPrice = getById("buy-price").value;
   const salePrice = getById("sale-price").value;
   if (!buyDate || !saleDate || !buyPrice || !salePrice) {
     if (!buyDate) {
-      restartAnimate(buyDateId);
+      startAnimate(inputIds.buyDate);
     }
     if (!saleDate) {
-      restartAnimate(saleDateId);
+      startAnimate(inputIds.saleDate);
     }
     if (!buyPrice) {
-      restartAnimate(buyPriceInputContainer);
+      startAnimate(buyPriceInputContainer);
     }
     if (!salePrice) {
-      restartAnimate(salePriceInputContainer);
+      startAnimate(salePriceInputContainer);
     }
     return;
   }
@@ -119,10 +136,8 @@ async function calculateButtonClick() {
     buyPrice,
     salePrice
   );
+  resultContainer.scrollIntoView({ 
+    behavior: 'smooth' 
+  });
 }
-function restartAnimate(id) {
-  id.classList.remove("error");
-  setTimeout(function () {
-    id.classList.add("error");
-  }, 1);
-}
+
